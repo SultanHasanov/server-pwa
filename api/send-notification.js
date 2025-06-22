25e5f1c9f09ca900c0cf9d869f68dcbb75acb810
sultan <sultanhasanov987@gmail.com>
2025-06-22 18:38:24 +0300

    init5

--- api/send-notification.js
+++ api/send-notification.js
@@ -56,9 +56,9 @@ const PRIVATE_VAPID = 'F6ziOjNHikq24j6T6TquTfJMLfX7E6ryhr0F11hMztc';
 webpush.setVapidDetails("mailto:you@example.com", PUBLIC_VAPID, PRIVATE_VAPID);
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+   res.setHeader('Access-Control-Allow-Origin', 'https://chechnya-product.ru');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === "OPTIONS") return res.status(200).end();
   if (req.method !== "POST")
